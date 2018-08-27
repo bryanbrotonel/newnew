ed3a6b90ddd1c649ee70c2275bed8cdb15bb23df
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2018-08-26 17:00:43 -0700

    Added motive page layout

--- src/scenes/Motive/index.js
+++ src/scenes/Motive/index.js
@@ -14,8 +14,16 @@ export default class Motive extends React.Component {
   render() {
     return (
       <div className="container">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque nam temporibus perferendis, mollitia itaque veniam qui minima molestias officiis necessitatibus dicta nihil recusandae labore ducimus at in ipsum quia. Illum.
-        </p>
+        <div className="row justify-content-center text-center">
+          <div className="col-sm-12 pb-3">
+            <h4>New New</h4>
+          </div>
+          <div className="col-sm-5">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque nam temporibus perferendis, mollitia itaque veniam qui minima molestias officiis necessitatibus dicta nihil recusandae labore ducimus at in ipsum quia. Illum.
+            </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, reiciendis, ipsa. Temporibus quibusdam, officiis molestiae quam voluptate omnis iste tempora voluptatum magnam accusantium! Repellendus possimus enim quos officiis, sed deserunt.</p>
+          </div>
+        </div>
       </div>
     )
   }
